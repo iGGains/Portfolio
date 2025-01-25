0fe8f2a273608499333ffefa5842e292cd6631c1
Isaiah Gains <107810544+iGGains@users.noreply.github.com>
2025-01-25 06:53:07 -0600

    Update Credit Default Swap Simulation Code (Hw 2 FINA).R

--- Credit Default Swap Simulation Code (Hw 2 FINA).R
+++ Credit Default Swap Simulation Code (Hw 2 FINA).R
@@ -105,70 +105,30 @@ comb3 <- dup_x1_3 + dup_x2_3
 comb4 <- dup_x1_4 + dup_x2_4
 comb5 <- dup_x1_5 + dup_x2_5
 
-#Replacement to convert matrix to 1s and 0s
-comb1[ comb1 < 2 ] <- 0
-comb2[ comb2 < 2 ] <- 0
-comb3[ comb3 < 2 ] <- 0
-comb4[ comb4 < 2 ] <- 0
-comb5[ comb5 < 2 ] <- 0
-
-comb1[ comb1 == 2 ] <- 1
-comb2[ comb2 == 2 ] <- 1
-comb3[ comb3 == 2 ] <- 1
-comb4[ comb4 == 2 ] <- 1
-comb5[ comb5 == 2 ] <- 1
-
 #Here are our probabilities:
-p_both_def1 <- length(comb1[ comb1 == 1 ])/100000
-p_both_def2 <- length(comb2[ comb2 == 1 ])/100000
-p_both_def3 <- length(comb3[ comb3 == 1 ])/100000
-p_both_def4 <- length(comb4[ comb4 == 1 ])/100000
-p_both_def5 <- length(comb5[ comb5 == 1 ])/100000
+p_both_def1 <- length(comb1[ comb1 == 2 ])/100000
+p_both_def2 <- length(comb2[ comb2 == 2 ])/100000
+p_both_def3 <-length(comb3[ comb3 == 2 ])/100000
+p_both_def4 <-length(comb4[ comb4 == 2 ])/100000
+p_both_def5 <-length(comb5[ comb5 == 2 ])/100000
 
 #Standard error
-se_both1 <- 1/sqrt(100000)*sd(comb1)
-se_both2 <- 1/sqrt(100000)*sd(comb2)
-se_both3 <- 1/sqrt(100000)*sd(comb3)
-se_both4 <- 1/sqrt(100000)*sd(comb4)
-se_both5 <- 1/sqrt(100000)*sd(comb5)
+1/sqrt(100000)*sd(comb1)
+1/sqrt(100000)*sd(comb2)
+1/sqrt(100000)*sd(comb3)
+1/sqrt(100000)*sd(comb4)
+1/sqrt(100000)*sd(comb5)
 
 #Question 3 
-#Start off by creating new combination matrix for either defaulting
-comb3_1 <- dup_x1_1 + dup_x2_1 
-comb3_2 <- dup_x1_2 + dup_x2_2
-comb3_3 <- dup_x1_3 + dup_x2_3
-comb3_4 <- dup_x1_4 + dup_x2_4
-comb3_5 <- dup_x1_5 + dup_x2_5
-
-#Replacement to convert matrix to 1s and 0s...note: (we want either defaulting)
-comb3_1[ comb3_1 < 1 ] <- 0
-comb3_2[ comb3_2 < 1 ] <- 0
-comb3_3[ comb3_3 < 1 ] <- 0
-comb3_4[ comb3_4 < 1 ] <- 0
-comb3_5[ comb3_5 < 1 ] <- 0
-
-comb3_1[ comb3_1 >= 1 ] <- 1
-comb3_2[ comb3_2 >= 1 ] <- 1
-comb3_3[ comb3_3 >= 1 ] <- 1
-comb3_4[ comb3_4 >= 1 ] <- 1
-comb3_5[ comb3_5 >= 1 ] <- 1
-
-#Here are our probabilities:
 #In order to find the probability of either defaulting, we modify the last portion of code from question 2.
 #In order to get the probability of either defaulting, we account for the 1s and 2s in the matrix. 0 means neither defaults
+P_either_def1 <- length(comb1[ comb1 >= 1 ])/100000
+P_either_def2 <- length(comb2[ comb2 >= 1 ])/100000
+P_either_def3 <- length(comb3[ comb3 >= 1 ])/100000
+P_either_def4 <- length(comb4[ comb4 >= 1 ])/100000
+P_either_def5 <- length(comb5[ comb5 >= 1 ])/100000
 
-p_either_def1 <- length(comb3_1[ comb3_1 == 1 ])/100000
-p_either_def2 <- length(comb3_2[ comb3_2 == 1 ])/100000
-p_either_def3 <- length(comb3_3[ comb3_3 == 1 ])/100000
-p_either_def4 <- length(comb3_4[ comb3_4 == 1 ])/100000
-p_either_def5 <- length(comb3_5[ comb3_5 == 1 ])/100000
 
-#Standard Errors
-se_either1 <- 1/sqrt(100000)*sd(comb3_1)
-se_either2 <- 1/sqrt(100000)*sd(comb3_2)
-se_either3 <- 1/sqrt(100000)*sd(comb3_3)
-se_either4 <- 1/sqrt(100000)*sd(comb3_4)
-se_either5 <- 1/sqrt(100000)*sd(comb3_5)
 
 #Question 1
 #Probab
@@ -192,84 +152,12 @@ p_both_def3
 p_both_def4
 p_both_def5 
 
-#Standard error
-se_both1 
-se_both2 
-se_both3
-se_both4
-se_both5
-
 #Question 3
-p_either_def1 
-p_either_def2 
-p_either_def3 
-p_either_def4
-p_either_def5 
-
-#Standard error
-se_either1 
-se_either2 
-se_either3 
-se_either4 
-se_either5 
+P_either_def1 
+P_either_def2 
+P_either_def3 
+P_either_def4
+P_either_def5 
 
 #Question 4
-
-no_def <- rbinom(10,0,.04)
-count <- 0
-for (val in no_def) {
-  if(val == 1)  count = count+1
-  repeat()
- 
-}
-print(count)
-
-
-two_def <- rbinom(10,2,.04)
-iterations <- rep(two_def,100000)
-count2 <- 0
-for (rep in two_def) {
-  if(sum(two_def) == 1)  count2 = count2+1
-}
-print(count2)
-
-
-
-
-
-
-n_sim <- 5
-p_def <- rbinom(10,2,.04)
-result <- rep(p_def,n_sim)
-count <- 0
-for (i in 1:n_sim) {
-  if(sum(p_def) == 2) count = count+1 
-  result[i] <- i
-}
-result
-count
-
-
-n_sim <- 10
-p_def <- rbinom(10,2,.04)
-result <- replicate(n_sim,p_def)
-count <- 0
-for (i in 1:n_sim) {
-  if(sum(p_def) == 2) count = count+1 
-  result[i] <- i
-}
-result
-count
-
-rbinom(10,2,.04)
-rep(rbinom(10,2,.04),4)
-
-repeat {
-  rbinom(10,2,.04)
-  
-  ()
-replicate(5,rbinom(10,2,.04))
-
-rbinom(50000, 10, 0.3)
-
-hist(replicate(10,runif(10)))
+asset1 <- rbinom(100000,10,.04)
